861d3c7477b421b9bc0ef4a4fe0ad31d4b6623b1
Matthew-Tian5 <matthew.tian2005@gmail.com>
2025-07-08 16:24:48 -0400

    final version of code

--- StudentMain.java
+++ StudentMain.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
-
 /*
  * 
  * Main file, reads each file, splits the names and then loops through all IDS to check if they match
@@ -19,50 +17,43 @@ import java.io.IOException;
  */
 public class StudentMain {
 
-    
+    public static void main(final String[] args) throws FileNotFoundException, IOException {
+        Scanner courseFile = new Scanner(new File("CourseFile.txt"));
+        Scanner nameFile = new Scanner(new File("NameFile.txt"));
 
-
-    public static void main(final String[] args) throws FileNotFoundException, IOException{
-        Scanner courseFile = new Scanner (new File("StudentFormatter/CourseFile.txt"));
-        Scanner nameFile = new Scanner (new File("StudentFormatter/NameFile.txt"));
-        
-     
         ArrayList<Student> studentList = new ArrayList<Student>();
         ArrayList<String> names = new ArrayList<>();
         ArrayList<String> courses = new ArrayList<>();
 
-        
-        while (nameFile.hasNextLine()){
+        while (nameFile.hasNextLine()) {
             names.add(nameFile.nextLine().trim());
         }
-        while (courseFile.hasNextLine()){
+        while (courseFile.hasNextLine()) {
             courses.add(courseFile.nextLine().trim());
-        }      
-
-        for (String nameLine: names){
-            String compName = nameLine.split(",")[0].trim();
-            for (String courseLine: courses){
-                String compID = courseLine.split(",")[0].trim();
-                if (compID.equals(compName)){
-                    studentList.add(StudentUtilities.combineAll(courseLine,nameLine));
-                } 
-            }
-            
         }
-    
-        
+
+        for (String nameLine : names) {
+            String compName = nameLine.split(",")[0].trim();
+            for (String courseLine : courses) {
+                String compID = courseLine.split(",")[0].trim();
+                if (compID.equals(compName)) {
+                    studentList.add(StudentUtilities.combineAll(courseLine, nameLine));
+                }
+            }
+
+        }
 
         studentList.sort(null);
         FileWriter formattedFile = new FileWriter("StudentDatabase.txt");
-        formattedFile.write("Student ID | Student Name         | Course Code | Final Grade \n" );
-        formattedFile.write ("------------------------------------------------------------\n");
+        formattedFile.write("Student ID | Student Name         | Course Code | Final Grade \n");
+        formattedFile.write("------------------------------------------------------------\n");
         for (Student s : studentList) {
-            formattedFile.write(s +"\n" );
+            formattedFile.write(s + "\n");
         }
-    
+
         courseFile.close();
         nameFile.close();
         formattedFile.close();
     }
-    
+
 }
